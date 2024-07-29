aba0ba6b7e764dcf2283007fedbc382f7df51c62
Max M <max.mckinley@fortra.com>
2024-07-29 13:31:44 -0500

    add token file

---
+++ src/token.rs
@@ -0,0 +1,44 @@
+use core::fmt;
+
+use serde::Serialize;
+
+#[derive(Debug, PartialEq, Eq, Serialize, Clone, Copy)]
+#[serde(rename_all = "kebab-case")]
+pub enum Class {
+  Identifier, Keyword,
+  String, Number, 
+  Boolean, Null,
+
+  LeftBrace, RightBrace, 
+  LeftBrack, RightBrack, 
+  LeftParen, RightParen,
+
+  Dot, Comma, Colon, SemiColon,
+  BinaryOp, BooleanOp, LogicOp,
+  Eof, Assign, Arrow, 
+}
+
+impl fmt::Display for Class {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{}", serde_yaml::to_string(self).unwrap().replace("\n", ""))
+  }
+}
+
+#[derive(Debug, PartialEq, Eq, Serialize, Clone)]
+pub struct Token {
+  text: String,
+  class: Class,
+  index: [usize; 2]
+}
+
+impl Token {
+  pub fn new<S:ToString>(class: Class, text: S, index: [usize; 2]) -> Token {
+    Self { text: text.to_string(), class, index }
+  }
+}
+
+impl fmt::Display for Token {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "token:{}<text:{:?}, index: {:?}>", self.class, self.text, self.index)
+  }
+}
